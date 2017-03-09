4b6756391db5a72554d8fbd80fedc936d09dcef5
neymarsabin <reddevil.sabin@gmail.com>
2017-03-09 16:43:08 +0545

    add about packages

---
+++ first/student.java
@@ -0,0 +1,11 @@
+package first ;
+public class student {
+		String name;
+		public void display() {
+				System.out.println("hello from packages");
+		}
+		public static void main(String[] args ) {
+				// main code here 
+		}
+}
+		
---
+++ person.java
@@ -0,0 +1,8 @@
+import first.*;
+
+class person {
+		public static void main(String[] args ) {
+				student s1 = new student();
+				s1.display();
+		}
+}
